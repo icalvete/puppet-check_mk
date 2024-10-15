03772fd90112b91a3f03650addd9b53fbe789176
Israel Calvete <icalvete@gmail.com>
2024-10-15 06:20:07 +0000

    new

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class check_mk::params {
 
   $repo_scheme = hiera('sp_repo_scheme', 'http')
-  $repo_domain = hiera('sp_repo_domain', 'smarpurposes.s3-website-eu-west-1.amazonaws.com')
+  $repo_domain = hiera('sp_repo_domain', 'smarpurposes.s3.eu-west-1.amazonaws.com')
   $repo_port   = hiera('sp_repo_port', '')
   $repo_user   = hiera('sp_repo_user', '')
   $repo_pass   = hiera('sp_repo_pass', '')
